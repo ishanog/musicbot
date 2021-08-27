9d853c7cd05aee3365018f2b619dc2fe0436f6e6
ishanog <88334650+ishanog@users.noreply.github.com>
2021-08-27 15:34:16 +0545

    Add files via upload

---
+++ Music-bot-master/Music-bot-master/config/bot.js
@@ -0,0 +1,18 @@
+module.exports = {
+emojis: {
+    off: ':x:',
+    error: ':warning:',
+    queue: ':bar_chart:',
+    music: ':musical_note:',
+    success: ':white_check_mark:',
+}
+
+
+    discord: {
+        token: 'Nzg2OTM4Mjc3NDc5MDU1Mzcw.X9NrYQ.wmZrV9klKX_SAw7_vxysR-j8_Uk',
+        prefix: 'mb',
+        activity: 'PLAYING',
+    },
+
+    filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
+};
